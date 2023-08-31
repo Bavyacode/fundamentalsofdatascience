29eac4e247cc4f72a8cf8055ff12d37cb1601cae
Bavyacode <129872721+Bavyacode@users.noreply.github.com>
2023-08-31 08:27:05 +0530

    Add files via upload

---
+++ fods36.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+stock_data = pd.read_csv("C:/Users/bhara/Downloads/studies/work/New folder/FODS/36.finace data.csv")
+# Calculate the daily price changes
+stock_data['PriceChange'] = stock_data['ClosingPrice'].diff()
+
+mean_change = stock_data['PriceChange'].mean()
+std = stock_data['PriceChange'].std()
+
+print("Stock Price Variability Analysis")
+print(f"Mean Daily Price Change: {mean_change:.2f}")
+print(f"Standard Deviation of Daily Price Changes: {std:.2f}")
+#Determine the direction of stock movement
+positive_changes = stock_data[stock_data['PriceChange'] > 0]['PriceChange'].count()
+negative_changes = stock_data[stock_data['PriceChange'] < 0]['PriceChange'].count()
+
+print("\nStock Movement Direction")
+print(f"Days with Positive Price Change: {positive_changes}")
+print(f"Days with Negative Price Change: {negative_changes}")
