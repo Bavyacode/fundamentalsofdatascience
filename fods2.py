d275f907e7ee45897fc2fa352fd294f40af6cdf7
Bavyacode <129872721+Bavyacode@users.noreply.github.com>
2023-08-31 09:25:44 +0530

    Delete fods2.py

--- fods2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-sales_data = np.array([input("Enter row separated by spaces for product {} sales: ".format(i+1)).split() for i in range(3)], dtype=int)
-
-average_price = np.mean(sales_data)
-
-print("Average price of all products sold in the past month:", average_price)
